75c3b34272ee5adf5729a85d285f114c2505e004
Astlo <lebars.yannis@gmail.com>
2016-10-30 21:03:52 +0100

    Model

---
+++ src/Model/Car.cpp
@@ -0,0 +1,280 @@
+/**
+ * @file Car.cpp
+ * @author HUNAULT Marion & LE BARS Yannis
+ * @brief Définition des méthodes de la classe Car.
+**/
+
+#include "Car.hpp"
+#include <sstream> //pour les conversions en chaine
+
+using namespace std;
+
+//CONSTRUCTEUR
+//------------------------------------------------------------------------------
+Car::Car()
+{
+	reset();
+}
+
+//DESTRUCTEUR
+//------------------------------------------------------------------------------
+Car::~Car()
+{}
+
+//METHODES
+//------------------------------------------------------------------------------
+void Car::reset()
+{
+	point_.getX() = Constants::X_BASE_TRAJECTOIRE;
+	point_.getY() = Constants::Y_BASE_TRAJECTOIRE;
+//	oldPoint_.getX() = point_.getX();
+//	oldPoint_.getY() = point_.getY();
+	vitesse_ = 0.0;
+	angle_ = 0.0;
+//	place_ = 0;
+//	secteur_ = 1;
+//	tour_ = 1;
+//	oldSecteur_ = 0;
+//	secteurAtteint_ = 1;
+	accelere_ = false;
+	freine_ = false;
+	left_ = false;
+	right_ = false;
+//	collision_ = 0;
+//	courseTerminee_ = false;
+//	etatGlissade_ = 0.0;
+//	etatSaut_ = 0.0;
+}
+
+//------------------------------------------------------------------------------
+string Car::toString() const
+{
+	stringstream sst;
+	sst <<"Voiture [Point=" << point_ /*<< ", oldPoint_=" << oldPoint_ */<< ", vitesse=" << vitesse_
+			<< ", angle=" << angle_ /*<< ", tour=" << tour_ << ", place=" << place_ << ", secteur=" << secteur_ << ", oldSecteur="
+			<< oldSecteur_ << ", secteurAtteint=" << secteurAtteint_ */<< ", accelere=" << accelere_ << ", freine=" << freine_
+			<< ", left=" << left_ << ", right=" << right_ /*<< ", collision=" << collision_ << ", courseTerminee="
+			<< courseTerminee_ << ", etatGlissade=" << etatGlissade_ << ", angleGlissade=" << angleGlissade_ << ", etatSaut="
+			<< etatSaut_ */<< "]"
+	return sst.str();
+}
+
+//ACCESSEURS
+//------------------------------------------------------------------------------
+Point Car::getPoint() const
+{
+	return point_;
+}
+/*
+//------------------------------------------------------------------------------
+Point Car::getOldPoint() const
+{
+	return oldPoint_;
+}
+*/
+//------------------------------------------------------------------------------
+double Car::getVitesse() const
+{
+	return vitesse_;
+}
+
+//------------------------------------------------------------------------------
+double Car::getAngle() const
+{
+	return angle_;
+}
+/*
+//------------------------------------------------------------------------------
+int Car::getTour() const
+{
+	return tour_;
+}
+
+//------------------------------------------------------------------------------
+int Car::getPlace() const
+{
+	return place_;
+}
+
+//------------------------------------------------------------------------------
+int Car::getSecteur() const
+{
+	return secteur_;
+}
+
+//------------------------------------------------------------------------------
+int Car::getOldSecteur() const
+{
+	return oldSecteur_;
+}
+
+//------------------------------------------------------------------------------
+int Car::getSecteurAtteint() const
+{
+	return secteurAtteint_;
+}
+*/
+//------------------------------------------------------------------------------
+bool Car::isAccelere() const
+{
+	return accelere_;
+}
+
+//------------------------------------------------------------------------------
+bool Car::isFreine() const
+{
+	return freine_;
+}
+
+//------------------------------------------------------------------------------
+bool Car::isLeft() const
+{
+	return left_;
+}
+
+//------------------------------------------------------------------------------
+bool Car::isRight() const
+{
+	return right_;
+}
+/*
+//------------------------------------------------------------------------------
+int Car::getCollision() const
+{
+	return collision_;
+}
+
+//------------------------------------------------------------------------------
+bool Car::isCourseTerminee() const
+{
+	return courseTerminee_;
+}
+
+//------------------------------------------------------------------------------
+double Car::getEtatGlissade() const
+{
+	return etatGlissade_;
+}
+
+//------------------------------------------------------------------------------
+double Car::getEtatSaut() const
+{
+	return etatSaut_;
+}
+
+//------------------------------------------------------------------------------
+double Car::getAngleGlissade() const
+{
+	return angleGlissade_;
+}
+*/
+//MUTATEURS
+//------------------------------------------------------------------------------
+void Car::setPoint(Point point)
+{
+	point_ = point;
+}
+/*
+//------------------------------------------------------------------------------
+void Car::setOldPoint(double oldPoint)
+{
+	oldPoint_ = oldPoint;
+}
+*/
+//------------------------------------------------------------------------------
+void Car::setVitesse(double vitesse)
+{
+	vitesse_ = vitesse;
+}
+
+//------------------------------------------------------------------------------
+void Car::setAngle(double angle)
+{
+	angle_ = angle;
+
+}
+/*
+//------------------------------------------------------------------------------
+void Car::setTour(int tour)
+{
+	tour_ = tour;
+}
+
+//------------------------------------------------------------------------------
+void Car::setPlace(int place)
+{
+	place_ = place;
+}
+
+//------------------------------------------------------------------------------
+void Car::setSecteur(int secteur)
+{
+	secteur_ = secteur;
+}
+
+//------------------------------------------------------------------------------
+void Car::setOldSecteur(int oldSecteur)
+{
+	oldSecteur_ = oldSecteur;
+}
+
+//------------------------------------------------------------------------------
+void Car::setSecteurAtteint(int secteurAtteint)
+{
+	secteurAtteint_ = secteurAtteint;
+}
+*/
+//------------------------------------------------------------------------------
+void Car::setAccelere(bool accelere)
+{
+	accelere_ = accelere;
+}
+
+//------------------------------------------------------------------------------
+void Car::setFreine(bool freine)
+{
+	freine_ = freine;
+}
+
+//------------------------------------------------------------------------------
+void Car::setLeft(bool left)
+{
+	left_ = left;
+}
+
+//------------------------------------------------------------------------------
+void Car::setRight(bool right)
+{
+	right_ = right;
+}
+/*
+//------------------------------------------------------------------------------
+void Car::setCollision(int collision)
+{
+	collision_ = collision;
+}
+
+//------------------------------------------------------------------------------
+void Car::setCourseTerminee(bool courseTerminee)
+{
+	courseTerminee_ = courseTerminee;
+}
+
+//------------------------------------------------------------------------------
+void Car::setEtatGlissade(double etatGlissade)
+{
+	etatGlissade_ = etatGlissade;
+}
+
+//------------------------------------------------------------------------------
+void Car::setAngleGlissade(double angleGlissade)
+{
+	angleGlissade_ = angleGlissade;
+}
+
+//------------------------------------------------------------------------------
+void Car::setEtatSaut(double etatSaut)
+{
+	etatSaut_ = etatSaut;
+}
+*/
